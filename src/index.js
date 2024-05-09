78e31378e63d15805fd8fc6da4e793c71f16993f
qwezsd <avwds@naver.com>
2024-05-09 14:45:23 +0900

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WrapComponent from './wrap/WrapComponent';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-      <WrapComponent />
-);
--- src/wrap/wrapContext/WrapContext.jsx
+++
@@ -1,2 +0,0 @@
-import { createContext } from "react";
-export const WrapContext = createContext(null);
