71e59b2c544c4c2e744050a6da9c61697905b2dc
qwezsd <avwds@naver.com>
2024-05-09 11:45:18 +0900

    마켓컬리

--- src/wrap/HeaderComponent.jsx
+++ src/wrap/HeaderComponent.jsx
@@ -96,7 +96,7 @@ export default function HeaderComponent () {
                         </div>
                         <div className="row2">
                             <div className="left">
-                                <a href="/main"><img src="./img/header/main_title.svg" alt="" /></a>
+                                <a href="../../public/index.html"><img src="./img/header/main_title.svg" alt="" /></a>
                                 <h3>마켓컬리</h3>
                                 <i>|</i>
                                 <h3 className='beauty'>뷰티컬리</h3>
